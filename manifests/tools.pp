ea964d18b3e1260a67c403fdf4991d579183e779
Jeff Scelza <jeffscelza76@gmail.com>
2015-09-08 13:37:01 -0600

    Had the wrong extension

--- manifests/tools.pp
+++ manifests/tools.pp
@@ -17,7 +17,7 @@ class adobe_em6::tools {
 
   file { "${adobe_em6::params::dir_tool}/aem_bundle_status.rb":
     ensure  => 'present',
-    source  => 'puppet:///modules/adobe_em6/aem_bundle_status.rb.erb',
+    source  => 'puppet:///modules/adobe_em6/aem_bundle_status.rb',
     require => File[ $adobe_em6::params::dir_tools ],
   }
 
