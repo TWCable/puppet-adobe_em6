5b881daf8235718009798074d3a00618391dcd76
Jeff Bornemann <jeff.bornemann@i-cubed.com>
2015-09-30 11:20:23 -0600

    Require ruby and required gem

--- manifests/tools.pp
+++ manifests/tools.pp
@@ -15,10 +15,25 @@
 #
 class adobe_em6::tools {
 
+  package { 'ruby' :
+    ensure => '1.8.7.374-4.el6_6'
+  }
+
+  package { 'rubygems' :
+    ensure => '1.3.7-5.el6',
+    require => Package[ 'ruby' ]
+  }
+
+  package { 'json_pure' :
+    provider => 'gem',
+    ensure => "1.8.2",
+    require => Package[ 'ruby' ]
+  }
+
   file { "${adobe_em6::params::dir_tools}/aem_bundle_status.rb":
     ensure  => 'present',
     source  => 'puppet:///modules/adobe_em6/aem_bundle_status.rb',
-    require => File[ $adobe_em6::params::dir_tools ],
+    require => [ File[ $adobe_em6::params::dir_tools ], Package[ 'ruby' ], Package[ 'json_pure' ] ]
   }
 
 }
